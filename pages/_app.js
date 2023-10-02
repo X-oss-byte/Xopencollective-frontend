13330aacd005f1121b41b8646890377836db1bc8
Benjamin Piouffle <benjamin@opencollective.com>
2023-10-02 15:34:24 +0200

    enhancement(SSR): iterate on setup

--- pages/_app.js
+++ pages/_app.js
@@ -36,6 +36,9 @@ import memoizeOne from 'memoize-one';
 
 import { APOLLO_STATE_PROP_NAME, initClient } from '../lib/apollo-client';
 import { getGoogleMapsScriptUrl, loadGoogleMaps } from '../lib/google-maps';
+import TwoFactorAuthProvider, {
+  withTwoFactorAuthentication,
+} from '../lib/two-factor-authentication/TwoFactorAuthenticationContext';
 import sentryLib from '../server/sentry';
 
 import GlobalNewsAndUpdates from '../components/GlobalNewsAndUpdates';
@@ -47,6 +50,7 @@ import { TooltipProvider } from '../components/ui/Tooltip';
 
 class OpenCollectiveFrontendApp extends App {
   static propTypes = {
+    twoFactorAuthContext: PropTypes.object,
     pageProps: PropTypes.object.isRequired,
     scripts: PropTypes.object.isRequired,
     locale: PropTypes.string,
@@ -116,33 +120,38 @@ class OpenCollectiveFrontendApp extends App {
   }
 
   getApolloClient = memoizeOne(pageProps => {
-    return initClient({ initialState: pageProps[APOLLO_STATE_PROP_NAME] });
+    return initClient({
+      initialState: pageProps[APOLLO_STATE_PROP_NAME],
+      twoFactorAuthContext: this.props.twoFactorAuthContext,
+    });
   });
 
   render() {
     const { Component, pageProps, scripts, locale } = this.props;
     return (
       <Fragment>
-        <ApolloProvider client={this.getApolloClient(pageProps)}>
-          <ThemeProvider theme={theme}>
-            <StripeProviderSSR>
-              <IntlProvider locale={locale}>
-                <TooltipProvider delayDuration={500} skipDelayDuration={100}>
-                  <ToastProvider>
-                    <UserProvider>
-                      <NewsAndUpdatesProvider>
-                        <Component {...pageProps} />
-                        <GlobalToasts />
-                        <GlobalNewsAndUpdates />
-                        <TwoFactorAuthenticationModal />
-                      </NewsAndUpdatesProvider>
-                    </UserProvider>
-                  </ToastProvider>
-                </TooltipProvider>
-              </IntlProvider>
-            </StripeProviderSSR>
-          </ThemeProvider>
-        </ApolloProvider>
+        <TwoFactorAuthProvider>
+          <ApolloProvider client={this.getApolloClient(pageProps)}>
+            <ThemeProvider theme={theme}>
+              <StripeProviderSSR>
+                <IntlProvider locale={locale}>
+                  <TooltipProvider delayDuration={500} skipDelayDuration={100}>
+                    <ToastProvider>
+                      <UserProvider>
+                        <NewsAndUpdatesProvider>
+                          <Component {...pageProps} />
+                          <GlobalToasts />
+                          <GlobalNewsAndUpdates />
+                          <TwoFactorAuthenticationModal />
+                        </NewsAndUpdatesProvider>
+                      </UserProvider>
+                    </ToastProvider>
+                  </TooltipProvider>
+                </IntlProvider>
+              </StripeProviderSSR>
+            </ThemeProvider>
+          </ApolloProvider>
+        </TwoFactorAuthProvider>
         <DefaultPaletteStyle palette={defaultColors.primary} />
         {Object.keys(scripts).map(key => (
           <script key={key} type="text/javascript" src={scripts[key]} />
@@ -152,4 +161,4 @@ class OpenCollectiveFrontendApp extends App {
   }
 }
 
-export default OpenCollectiveFrontendApp;
+export default withTwoFactorAuthentication(OpenCollectiveFrontendApp);
